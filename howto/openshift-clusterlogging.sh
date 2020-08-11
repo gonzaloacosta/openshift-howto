e1036d12ab4d63052cf833dd5aea8b4e7219510d
Gonzalo Acosta <gonzaloacostapeiro@gmail.com>
2020-08-11 16:20:59 -0300

    agrego notas para elastic search

--- howto/openshift-clusterlogging.sh
+++ howto/openshift-clusterlogging.sh
@@ -1,51 +1,9 @@
 # Chequeos de elasticsearch para Openshift
-
-#how to forward logs to external rsyslog in openshift 4.3?
-https://access.redhat.com/solutions/4931831
-
-#Can't access to kibana in ocp4 http: TLS handshake error from IP: remote error: tls: unknown certificate authority
-https://access.redhat.com/solutions/4963621
-
-#Supported versions of EFK stack in OCP 3.x and OCP 4.x.
-https://access.redhat.com/solutions/4850671
-
-#Setting Fluentd pod logs to debug level in OCP 4.x
-https://access.redhat.com/solutions/4661421
-
-#Fluentd buffer logs are causing pod evictions in OpenShift 4
-https://access.redhat.com/solutions/4801291
-
-#Este ultimo problema es el que hemos tenido en ITAU y GIRE
-
-#Ajustes avanzados: encontrar y resolver búsquedas lentas en Elasticsearch
-https://www.elastic.co/es/blog/advanced-tuning-finding-and-fixing-slow-elasticsearch-queries
-
-#DiskPressure due to 80 GB /var/lib/fluentd
-https://bugzilla.redhat.com/show_bug.cgi?id=1780698
-
-oc get pod fluentd-z8fx7 -o yaml |grep BUFFER -A 2
-    - name: BUFFER_QUEUE_LIMIT
-      value: "32"
-    - name: BUFFER_SIZE_LIMIT
-      value: 8m
-    - name: FILE_BUFFER_LIMIT
-      value: 256Mi
-    - name: FLUENTD_CPU_LIMIT
-
-oc exec fluentd-z8fx7 -- du -sh /var/lib/fluentd/clo_default_output_es/
-7.7G	/var/lib/fluentd/clo_default_output_es/
- oc logs fluentd-ff8nn
-2020-05-06 02:22:18 +0000 [warn]: [clo_default_output_es] failed to write data into buffer by buffer overflow action=:block
-
-#Dashboard de Grafana para ElasticSearch
-https://gitlab.com/rahasak-labs/prometheus-elasticsearch/-/blob/master/elastic-dashboard.json
-https://medium.com/rahasak/monitor-elasticsearch-with-prometheus-and-grafana-687a0b6712
-
-#Elasticsearch reporting IndexPrimaryShardNotAllocatedException and in RED state
-https://access.redhat.com/solutions/3768141
-
+#######################################################################################
 
 # Consultar estado de cluster de ES
+oc get pods | grep elasticsearch
+
 oc rsh $POD_NAME
 
 export ES_ARGS="-s --key /etc/elasticsearch/secret/admin-key --cert /etc/elasticsearch/secret/admin-cert --cacert /etc/elasticsearch/secret/admin-ca"
@@ -116,4 +74,54 @@ for line in $(curl $ES_ARGS https://localhost:9200/_cat/shards' | fgrep UNASSIGN
 '
 
 # Backup y Restore
+#######################################################################################
 # TBD
+
+
+# Notas principales para el troubleshooting
+#######################################################################################
+#how to forward logs to external rsyslog in openshift 4.3?
+https://access.redhat.com/solutions/4931831
+
+#Can't access to kibana in ocp4 http: TLS handshake error from IP: remote error: tls: unknown certificate authority
+https://access.redhat.com/solutions/4963621
+
+#Supported versions of EFK stack in OCP 3.x and OCP 4.x.
+https://access.redhat.com/solutions/4850671
+
+#Setting Fluentd pod logs to debug level in OCP 4.x
+https://access.redhat.com/solutions/4661421
+
+#Fluentd buffer logs are causing pod evictions in OpenShift 4
+https://access.redhat.com/solutions/4801291
+
+#Este ultimo problema es el que hemos tenido en ITAU y GIRE
+
+#Ajustes avanzados: encontrar y resolver búsquedas lentas en Elasticsearch
+https://www.elastic.co/es/blog/advanced-tuning-finding-and-fixing-slow-elasticsearch-queries
+
+#DiskPressure due to 80 GB /var/lib/fluentd
+https://bugzilla.redhat.com/show_bug.cgi?id=1780698
+
+oc get pod fluentd-z8fx7 -o yaml |grep BUFFER -A 2
+    - name: BUFFER_QUEUE_LIMIT
+      value: "32"
+    - name: BUFFER_SIZE_LIMIT
+      value: 8m
+    - name: FILE_BUFFER_LIMIT
+      value: 256Mi
+    - name: FLUENTD_CPU_LIMIT
+
+oc exec fluentd-z8fx7 -- du -sh /var/lib/fluentd/clo_default_output_es/
+7.7G	/var/lib/fluentd/clo_default_output_es/
+ oc logs fluentd-ff8nn
+2020-05-06 02:22:18 +0000 [warn]: [clo_default_output_es] failed to write data into buffer by buffer overflow action=:block
+
+#Dashboard de Grafana para ElasticSearch
+https://gitlab.com/rahasak-labs/prometheus-elasticsearch/-/blob/master/elastic-dashboard.json
+https://medium.com/rahasak/monitor-elasticsearch-with-prometheus-and-grafana-687a0b6712
+
+#Elasticsearch reporting IndexPrimaryShardNotAllocatedException and in RED state
+https://access.redhat.com/solutions/3768141
+
+
